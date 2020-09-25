410b156a97150985d622940b992bdffb946e5cf6
Jay Womack <womackjw@hotmail.com>
2020-09-24 17:22:43 -0700

    Finished jquery part two

--- DJANGO_COURSE_2.xx/10-jQuery/Part2_Events.js
+++ DJANGO_COURSE_2.xx/10-jQuery/Part2_Events.js
@@ -1,73 +1,37 @@
-// jQuery makes it easy to interact with the DOM!
+// Clicks
 
-// List of all possible events!
-// https://api.jquery.com/category/events/
+$('h1').click(function () {
+  console.log('There was a click!');
+});
 
-//////////////
-// CLICKS ///
-////////////
+$('li').click(function () {
+  console.log('any li was clicked');
+});
 
-// On Click
-$('h1').click(function(){
-  console.log("There was a click!");
-})
+// Effect what is being clicked
+$('h1').click(function () {
+  $(this).text('I was changed!');
+});
 
-// Click on multiple elements
-$('li').click(function() {
-  console.log("Click on any li !");
-})
+// Key press
 
-// Using This with jQuery
-$('h3').click(function() {
-  $(this).text("I was changed!");
-})
+$('input')
+  .eq(0)
+  .keypress(function () {
+    if (event.which === 13) {
+      $('h3').toggleClass('turnBlue');
+    }
+  });
 
-/////////////////
-// KEYPRESS ////
-///////////////
-// Using This with jQuery
-$('input').eq(0).keypress(function() {
-  $('h3').toggleClass("turnRed");
-})
+// on()
 
-// We can use this event object, that has a ton of information!
-$('input').eq(0).keypress(function(event) {
-  console.log(event);
-})
+$('h1').on('dblclick', function () {
+  $(this).toggleClass('turnBlue');
+});
 
-// Each Keyboard Key has a Keycode, for example Enter is 13
-$('input').eq(0).keypress(function(event) {
-  if(event.which === 13){
-    $('h3').toggleClass("turnRed");
-  }
-})
-
-////////////
-// ON() ///
-//////////
-
-// on() basically works like addEventListener()
-$('h1').on("dblclick",function() {
-  $('h1').addClass('turnBlue');
-})
-
-$('li').on('mouseenter',function() {
-  $(this).toggleClass('turnRed');
-})
-
-/////////////////////////////
-// EFFECTS and ANIMATIONS //
-///////////////////////////
-
-// http://api.jquery.com/category/effects/
-
-$('input').eq(1).val("FADE OUT EVERYTHING");
-
-$('input').eq(1).on("click",function(){
-  $(".container").fadeOut(3000) ;
-})
-
-
-$('input').eq(1).on("click",function(){
-  $(".container").slideUp(1000) ;
-})
+// Jquery Animation
+$('input')
+  .eq(1)
+  .on('click', function () {
+    $('.container').slideUp(3000);
+  });
